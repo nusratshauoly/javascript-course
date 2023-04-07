eae455c43f306bd5418b2ff775168e5505f87001
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-04-07 13:59:17 +0600

    basic fundamental part- 1

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,11 @@
+//video- 10: LECTURE: Values and Variables
+/* 1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console  */
+
+let country = "Portugal";
+let continent = "Europe";
+let population = 10;
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,44 @@
+// vid - 10: values and variables
+
+//variable: it is basically a box into which we can store a Value. so we give box a name. and into that box, with that label, we can store a value and then we can reference that variable over and over again.
+let firstName = 'jonas';
+console.log(firstName);
+
+
+//'jonas' is a value.
+console.log('jonas');
+console.log(23);
+//let firstName is a variable and value are being kept inside variable.
+let firstName = 'jonas';
+
+console.log(firstName);
+
+// way of write variable 
+// camelCase variableName
+let first = 'jonas';
+let firstNamePerson; //used mostly
+let first_name_person;
+let PI = 3.1415; //can use this uppercase cause ite constant
+
+// way to not write variable
+let 3years = 3;
+let jonas&matilda = 'JM';
+let new = 27; //reserved keyword
+let function = 27;
+let $function = 27;
+let _function = 27;
+let name = 'jonas'; //name is also a keyword
+let Person = 'jonas';
+
+// valid variable Name
+
+var myFirstJob = 'Programmer';
+var myCurrentJob = 'teacher';
+
+// don't write like this
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+/////////////////////////////////////////
+
+// video - 11: Data types
