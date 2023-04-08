fa073a4800f6001f94632944de96333a54270fa1
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-04-08 17:07:48 +0600

    basic js

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,11 +1,186 @@
 //video- 10: LECTURE: Values and Variables
+
 /* 1. Declare variables called 'country', 'continent' and 'population' and
 assign their values according to your own country (population in millions)
 2. Log their values to the console  */
 
 let country = "Portugal";
 let continent = "Europe";
-let population = 10;
+let population = 11;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+////////////////////////////////////////////
+
+// video- 11: LECTURE: Data Types
+
+/*1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console */
+ 
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//////////////////////////////////////////////
+
+//video- 13: LECTURE: let, const and var
+
+/* 1. Set the value of 'language' to the language spoken where you live (some
+    countries have multiple languages, but just choose one)
+    2. Think about which variables should be const variables (which values will never
+    change, and which might change?). Then, change these variables to const.
+    3. Try to change one of the changed variables now, and observe what happens */
+const country = "Bangladesh";
+const continent = "Asia";
+let language = "Bangla";
+language = 'English';
+const isIsland = true;
+isIsland = false;
+
+//////////////////////////////////////////////
+
+// video-14: LECTURE: Basic Operators
+/* 1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese. */
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+let description = country + 'is in ' + continent + ', and its ' + population + 'million people speak portuguese';
+console.log(description);
+//////////////////////////////////////////
+
+// video - 16: coding challenge - 1
+
+//Coding Challenge #1
+/* Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall. */
+
+// data 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2 ;
+const BMIJohn = massJohn / (heightJohn * heightJohn) ;
+// console.log(BMIMark, BMIJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// data 2
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2 ;
+const BMIJohn = massJohn / (heightJohn * heightJohn) ;
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+//////////////////////////////////////////////////////////////
+
+
+//video - 17: LECTURE: Strings and Template Literals
+/* 1. Recreate the 'description' variable from the last assignment, this time
+using the template literal syntax */
+
+let description1 = `${country} is in ${continent}, and its ${population}million people speak portuguese`;
+console.log(description1);
+
+
+////////////////////////////////////////
+//video - 18.
+//LECTURE: Taking Decisions: if / else Statements
+/* 1. If your country's population is greater that 33 million, log a string like this to the
+console: 'Portugal's population is above average'. Otherwise, log a string like
+'Portugal's population is 22 million below average' (the 22 is the average of 33
+minus the country's population)
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original */
+// let population = 22;
+let population = 13;
+if(population > 33){
+    console.log(`${country}'s population is above average`);
+}else{
+    console.log(`${country}'s population is ${33 - population} million below average`);
+}
+
+/////////////////////////////////////////
+//vid - 19: Coding Challenge #2
+/* Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!" */
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2 ;
+const BMIJohn = massJohn / (heightJohn * heightJohn) ;
+console.log(BMIMark, BMIJohn);
+
+if(BMIMark > BMIJohn){
+   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+}
+else{
+   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
+
+///////////////////////////////////////
+
+//video - 20 : type conversopn and coercion
+
+//LECTURE: Type Conversion and Coercion
+
+/* 1. Predict the result of these 5 operations without executing them:
+'9' - '5';
+'19' - '13' + '17';
+'19' - '13' + 17;
+'123' < 57;
+5 + 6 + '4' + 9 - 4 - 2;
+2. Execute the operations to check if you were right
+*/
+console.log('9' - '5'); // => 4
+console.log('19' - '13' + '17'); // => 617
+console.log('19' - '13' + 17); // => 23
+console.log('123' < 57);  // => false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // => 1143
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -42,3 +42,282 @@ let job2 = 'teacher';
 /////////////////////////////////////////
 
 // video - 11: Data types
+
+// the 7 primitive data types: number, string, boolean, undefined, null, symbol and big int.
+
+//1. number : let age = 27;
+// 2. strings: let firstName = "jonas" / 'jonas';
+// 3. boolean: let fullAge = true / false ;
+//4.undefined: let children;
+//5. null
+//6. symbol(ES2015): value that is unique and cannot be changed.
+//7. BigInt(ES2020): larger integers than the Number type can hold.
+
+// single line comment in code (cmd + /)
+// var myFirstJob = 'Programmer';
+
+/* 
+ double line comment
+ */
+//finding data types of a variable
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+console.log(typeof true);
+
+//changing the variable
+let javascriptIsFun = true;
+javascriptIsFun = 'Yes!';
+console.log(javascriptIsFun);
+
+// undefined(empty variable)
+let year;
+console.log(year);
+console.log(typeof year);
+
+// reassigning value in undefined variable
+year = 1991;
+console.log(typeof year);
+
+/////////////////////////////////////////////
+
+//video - 13. let, const and var
+// let variable (can reassign called mutable variable)
+let age = 30;
+age = 31;
+
+// const variable (cannot change - immutable variable)
+const birthYear = 1997;
+const job; //(cannot write like this, need a initial value to the variable)
+// when the variabe will change in the future should use let. and if it's a fixed value always use const.
+
+
+//////////////////////////////////////////
+
+// video- 14: basic operator
+
+//1. arithmatic operators
+//(-) minus operator
+const ageJonas = 2037 - 1991;
+const ageSarah = 2037 - 2018;
+console.log(ageJonas, ageSarah); // console.log e ekshathe onek gula value declare kora jay
+// ekahne value repeat hocche which is not good thats why we store the value in a variable
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+// other math operations
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);  //2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 = 8.
+
+// math operators: (+) plus operators
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// assignment operator
+// (=) equal operator;
+let x = 10 + 5;
+x += 10;  //it means x = x + 10 = 25; its called (shorthand method)
+x *= 4; //x = x * 4 = 100;
+x++; // x = x + 1;
+x--; // x = x - 1;
+console.log(x);
+
+
+// comparison operators to produce boolean values
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018); //we can calculate and comparison togather
+
+/////////////////////////////////////////////
+
+//video - 15: Operator-Precedence
+//mdn javascript operator precendence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5); //operators calculate from left-to-right
+
+let x, y; //assignment operator (right to left)
+x = y = 25 - 10 - 5;  // x = y = 10, x = 10
+console.log(x, y);
+
+// grouping operators executes first then the others
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+
+///////////////////////////////////////////
+
+// video - 16: coding challenge - 1
+
+//Coding Challenge #1
+/* Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall. */
+
+// data 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2 ;
+const BMIJohn = massJohn / (heightJohn * heightJohn) ;
+// console.log(BMIMark, BMIJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// data 2
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2 ;
+const BMIJohn = massJohn / (heightJohn * heightJohn) ;
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+////////////////////////////////////////////////////////
+
+// video - 17: strings and template literals
+
+// normal strings
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old' + job + '!';
+console.log(jonas);
+
+// write template literals (using backtick(``) and ${})
+const jonasNew = `i'm ${firstName} a ${year - birthYear} year old ${job}!`
+console.log(jonasNew);
+console.log(`just a regular string...`);
+
+// multiline with normal strings
+console.log('string with \n\
+multiple \n\
+line');
+
+// multiline strings with template literals
+console.group(`string with
+multiple lines`);
+
+//////////////////////////////////////
+
+// video- 18: Taking Decisions: if / else Statements
+
+// const age = 19;
+const age = 15;
+// const isOldEnough = age >= 18;
+if(age >= 18){
+   console.log('Sarah can start driving licence');
+}
+else{
+    const yearsLeft = 18 - age;
+   console.log(`sarah is too young, wait another ${yearsLeft} years`);
+}
+
+// 
+// const birthYear = 1991;
+const birthYear = 2011;
+let century;
+if(birthYear <= 2000){
+   century = 20;
+}else{
+   century = 21;
+}
+console.log(century);
+
+//////////////////////////////////////
+
+//vid - 19: Coding Challenge #2
+/* Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!" */
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2 ;
+const BMIJohn = massJohn / (heightJohn * heightJohn) ;
+console.log(BMIMark, BMIJohn);
+
+if(BMIMark > BMIJohn){
+   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+}
+else{
+   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
+
+///////////////////////////////////////
+
+//video - 20 : type conversopn and coercion
+
+/* type conversion: is when we manually convert from one type to another.
+
+type coercion: is when javascript automatically converts types behind the scenes for us.
+
+*/
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(inputYear + 18);
+
+// 
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+
+console.log('I am' + 23 + ' years old');
+//console.log('I am' + '23' + ' years old');
+
+console.log('23' + '10' + 3);
+console.log('23' - '10' - 3); // minus operators triggers the opposite conversion. so this case strings are converted to numbers.
+
+console.log('23' * '2'); //here the result will be shown as numbers because this way the multiplier operator can work
+
+console.log('23' / '2'); // same as multiply the answer will be number
+
+//gess the output
+let n = '1' + 1;
+n = n - 1;
+console.log(n); // answer : 11 => 11-1 = 10(number)
+
+// 2+ 3+ 4 + '5' answer: "95"
+
+//'10' - '4' - '3' - 2 + '5'  answer: '10'- '4' ='6' - '3' = '3' - 2 = 1 + '5' = '15'
+
