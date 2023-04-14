0bc6a381c4d6a57a880fcfbba29a5c0d68bc1cff
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-04-14 20:10:23 +0600

    video - 32: use strict

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,13 @@
 // video- 32: Activating strict mode
+
+// 'use strict'; mode on only for specific function or a specific block.  
+'use strict';
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true; 
+if(hasDriversLicense) console.log('i can drive :D');
+
+// const interface = 'Audio';
+const private = 534;
+const if = 23; // it is error 
