e79b06b404c06dcc77c7258fb8b920bc5742ab1b
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-04-19 21:30:54 +0600

    codingg challenge on functions

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -102,12 +102,33 @@ Hints:
 § To check if number A is at least double number B, check for A >= 2 * B.
 Apply this to the team's average scores � */
 
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4. 5));
 
+// test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
 
+let checkWinner = function(avgDolphins, avgKoalas){
+   if(avgDolphins >= 2 * avgKoalas){
+      console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+}
+    else if(avgKoalas >= 2 * avgDolphins){
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    }
+    else{
+        console.log('No team wins....');
+    }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(576, 111);
 
-
-
-
+// test 2
+scoreDolphins = calcAverage(85, 55, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
 
 
 
